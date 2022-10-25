4b03808642b3d4b1ba8b2b942710f3cebf75890e
chad fitzgerald <fitzchad@gmail.com>
2022-10-24 19:37:00 -0700

    Start w4d1 homework

---
+++ W4D1/graph_nodes_and_bfs.rb
@@ -0,0 +1,35 @@
+class GraphNode
+    attr_accessor :val, :neighbors
+
+    def initialize(val)
+        self.val = val
+        self.neighbors = []
+    end
+
+    def bfs(starting_node, target_val, visited=Set.new)
+        # return starting_node if starting_node.val == target_val
+        queue = [starting_node]
+
+        visited.add(starting_node.val)
+
+        until queue.empty?
+            starting_node = queue.pop
+            return node if node.value == target_value
+            starting_node.neighbors.each {|neighbor| queue.unshift(neighbor)}
+            bfs(starting_node, target_val, visited)
+        end
+    end
+end
+
+a = GraphNode.new('a')
+b = GraphNode.new('b')
+c = GraphNode.new('c')
+d = GraphNode.new('d')
+e = GraphNode.new('e')
+f = GraphNode.new('f')
+a.neighbors = [b, c, e]
+c.neighbors = [b, d]
+e.neighbors = [a]
+f.neighbors = [e]
+
+p bfs(a, "b")
