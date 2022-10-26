9a5e138cb49bffd921f1d7ae0339879837d99cec
chad fitzgerald <fitzchad@gmail.com>
2022-10-25 18:41:49 -0700

    Finish simon says

--- W4D3/skeleton/lib/simon.rb
+++ W4D3/skeleton/lib/simon.rb
@@ -4,38 +4,50 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
-
+    take_turn until game_over == true
+    game_over_message
+    reset_game
   end
 
   def take_turn
-
+    if game_over == false
+      show_sequence
+      require_sequence
+      # if require_sequence == show_sequence
+    end
+    round_success_message
+    @sequence_length += 1
   end
 
   def show_sequence
-
+    add_random_color
   end
 
   def require_sequence
-
+    @seq
   end
 
   def add_random_color
-
+    @seq << COLORS.sample
   end
 
   def round_success_message
-
+    "SUCCESS"
   end
 
   def game_over_message
-
+    "GAME OVER"
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
