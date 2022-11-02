8bb6daba76e7bf1a4ce3634fc73aa24ee0f4edf4
chad fitzgerald <fitzchad@gmail.com>
2022-11-01 20:58:08 -0700

    Work on homework W5D3

--- W5D3/skeleton/plays.rb
+++ W5D3/skeleton/plays.rb
@@ -24,7 +24,7 @@ class Play
   end
 
   def find_by_playwright(name)
-    
+
   end
 
   def initialize(options)
@@ -59,21 +59,33 @@ class Play
 end
 
 class Playwright
-  
-  def self.all
+  attr_accessor :id, :name, :birth_year
 
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+    data.map {|datum| Playwright.new(datum)}
   end
 
   def self.find_by_name(name)
 
   end
 
-  def initialize
-
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
   end
 
   def update
-
+    raise "#{self} not in database" unless self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year, self.id)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
   end
 
   def get_plays
